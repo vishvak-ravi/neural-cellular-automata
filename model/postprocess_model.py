7025c7307d4e99726af761e030ba8cd2852bdd13
vish <vishravi03@gmail.com>
2025-05-22 07:39:11 -0400

    batch training

--- model/postprocess_model.py
+++ model/postprocess_model.py
@@ -4,12 +4,12 @@ from utils import CAGetBoard, to_onnx
 
 if __name__ == "__main__":
     # load and convert model
-    model_name = "mudkip"
+    model_name = "mudkip_persist"
     torch_model = CAGetBoard()
     torch_model.load_state_dict(
         torch.load(f"data/params/{model_name}.pt", map_location=torch.device("cpu"))
     )
-    to_onnx(torch_model)
+    to_onnx(torch_model, model_name)
 
     # validate onxx
     onnx_model = onnx.load(f"data/params/{model_name}.onnx")
